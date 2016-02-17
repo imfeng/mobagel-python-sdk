2cd87816312274d2fc07e8618a0e96a909686a7d
alan <alan4chen@kimo.com>
2016-02-17 20:32:24 +0800

    complete py2 py3 SendReport

--- pybagel/py2/hello.py
+++ pybagel/py2/hello.py
@@ -4,7 +4,3 @@ import sys
 def hello():
     print "hello! pybagel!"
     print sys.version_info
-
-
-if __name__ == "__main__":
-    hello()
--- pybagel/py3/__init__.py
+++ pybagel/py3/__init__.py
@@ -1,1 +1,2 @@
 from pybagel.py3.hello import *
+from pybagel.py3.Client import *
